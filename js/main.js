b8e99fa694bb8a2d95d89051a208196fc9cfc738
alexisdurieux <alexis.durieux94@gmail.com>
2016-11-17 21:41:52 +0100

    Clean up website

--- js/main.js
+++ js/main.js
@@ -57,12 +57,52 @@ function initLinkURL(){
   })
 }
 
+function initMap() {
+  var mapOptions = {
+    center: new google.maps.LatLng(39.260325, -39.065246),
+    zoom: 2,
+    minZoom: 2
+  };
+  var map = new google.maps.Map(document.getElementById("map"),
+      mapOptions);
+
+
+  var infowindow = new google.maps.InfoWindow();
+
+  $.getJSON("../data/voyages_data.json", function(data) {
+
+    for (var i = 0; i < data.length; i++) {
+      var coords = data[i].coordinates;
+      var latLng = new google.maps.LatLng(coords[1],coords[0]);
+
+
+      var marker = new google.maps.Marker({
+         position: latLng,
+         map: map,
+         title: data[i].properties.name
+      });
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          var contentString = '<div id="iw-container">'+
+          '<div id="iw-title" style="font-size: 1.75em; margin-left:10px;">'+ data[i].properties.name +'</div>'+
+          '<div class="infowindow-info">' +
+          (data[i].properties.tag ? '<div class="map-tag">' + data[i].properties.tag + '</div>' : "") +
+          '<div class="background-item-date">' + data[i].properties.date + '</div>' +
+          (data[i].properties.picture ? '<div class="img-container"><img src="' + data[i].properties.picture + '"/></div>' : "") +
+          '</div>' +
+          '</div>'+
+          '</div>';
+          infowindow.setContent(contentString);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+  });
+}
+
+
 
 
 initScrollIt();
 initFiltering();
 initLinkURL();
-
-window.onload = function(){
-  console.log('onload');
-}
